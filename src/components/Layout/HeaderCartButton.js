f403daab7eab004af17fbb1933262f06cc182977
gopalmishra047 <42767726+gopalmishra047@users.noreply.github.com>
2021-06-26 17:45:21 +0530

    animate cart button whenever total amount changes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
+  const [animateButton, setAnimateButton] = useState(false);
+  // const numberOfItemsInCart = ctx.items.reduce((currentNumber, item) => {
+  //   return currentNumber + item.amount;
+  // }, 0);
 
-  const numberOfItemsInCart = ctx.items.reduce((currentNumber, item) => {
-    return currentNumber + item.amount;
-  }, 0);
+  const totalAmount = ctx.totalAmount.toFixed(2);
+
+  const buttonClasses = `${classes.button} ${
+    animateButton ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setAnimateButton(true);
+    const timer = setTimeout(() => {
+      setAnimateButton(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>{numberOfItemsInCart}</span>
+      <span className={classes.badge}>{`$ ${totalAmount}`}</span>
     </button>
   );
 };
