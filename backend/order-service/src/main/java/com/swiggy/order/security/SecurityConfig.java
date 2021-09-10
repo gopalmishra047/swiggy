1759001f536cbc4f1ef052da18c086d8b971e4b4
gopalmishra047 <42767726+gopalmishra047@users.noreply.github.com>
2021-09-10 14:53:38 +0530

    create docker registry and dockerise the backend app using docker-compose

--- backend/order-service/src/main/java/com/swiggy/order/security/SecurityConfig.java
+++ backend/order-service/src/main/java/com/swiggy/order/security/SecurityConfig.java
@@ -4,28 +4,12 @@ import java.util.Arrays;
 import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/order/**")
-        .permitAll()
-        .and()
-        .cors()
-        .and()
-        .csrf()
-        .disable();
-  }
+public class SecurityConfig {
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
@@ -36,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     corsConfiguration.setAllowedHeaders(
         Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/order/**", corsConfiguration);
+    source.registerCorsConfiguration("/**", corsConfiguration);
     return source;
   }
 }
