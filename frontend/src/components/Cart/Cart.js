c3825c0536a2543a3bacbdfbf01f142fab9b96ae
gopalmishra047 <42767726+gopalmishra047@users.noreply.github.com>
2021-10-19 13:12:01 +0530

    add cart checkout form

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,29 +1,33 @@
-import React, { useContext } from 'react';
-import classes from './Cart.module.css';
-import Modal from '../UI/Modal';
-import CartContext from '../../store/cart-context';
-import CartItem from './CartItem';
+import React, { useContext, useState } from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
-const Cart = (props) => {
+const Cart = props => {
   const cartContext = useContext(CartContext);
-
+  const [showCheckoutForm, setShowCheckoutForm] = useState(false);
   const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
   const isCartEmpty = cartContext.items.length < 1;
 
-  const addCartItemHandler = (item) => {
+  const addCartItemHandler = item => {
     cartContext.addItem({ ...item, amount: 1 });
   };
-  const removeCartItemHandler = (id) => {
+  const removeCartItemHandler = id => {
     cartContext.removeItem(id);
+  };
+  const checkoutFormHandler = () => {
+    setShowCheckoutForm(true);
   };
 
   return (
     <Modal onClose={props.onHideCart}>
-      <ul className={classes['cart-items']}>
-        {cartContext.items.map((item) => (
+      <ul className={classes["cart-items"]}>
+        {cartContext.items.map(item => (
           <CartItem
             key={item.id}
-            name={item.name} 
+            name={item.name}
             amount={item.amount}
             price={item.price}
             onAdd={addCartItemHandler.bind(null, item)}
@@ -35,11 +39,20 @@ const Cart = (props) => {
         <span>Total Amount:</span>
         <span>{totalAmount}</span>
       </div>
+      {!isCartEmpty && showCheckoutForm && (
+        <Checkout onCancel={props.onHideCart} />
+      )}
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onHideCart}>
-          Close
-        </button>
-        {!isCartEmpty && <button className={classes.button}>Order</button>}
+        {!showCheckoutForm && (
+          <button className={classes["button--alt"]} onClick={props.onHideCart}>
+            Close
+          </button>
+        )}
+        {!isCartEmpty && !showCheckoutForm && (
+          <button className={classes.button} onClick={checkoutFormHandler}>
+            Order
+          </button>
+        )}
       </div>
     </Modal>
   );
--- frontend/src/components/Meals/AvailableMeals.js
+++ frontend/src/components/Meals/AvailableMeals.js
@@ -1,58 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-
+import React, { useEffect, useState } from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
-
   const [availableMeals, setAvailableMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState();
-  
+
   useEffect(() => {
     const fetchMeals = async () => {
-     const response = await fetch("http://localhost:8080/order/getAllMeals");
-     if(!response.ok){
-       throw new Error("Something went wrong !");
-     }
-     const responseData = await response.json();
-     const loadedMeals = [];
+      const response = await fetch("http://localhost:8000/order/getAllMeals");
+      if (!response.ok) {
+        throw new Error("Something went wrong !");
+      }
+      const responseData = await response.json();
+      const loadedMeals = [];
 
-     for(const meal in responseData){
-       loadedMeals.push({
-         id: responseData[meal].id,
-         name: responseData[meal].name,
-         description: responseData[meal].description,
-         price: responseData[meal].price
-       });
-       setIsLoading(false);
-     }
-     setAvailableMeals(loadedMeals);
+      for (const meal in responseData) {
+        loadedMeals.push({
+          id: responseData[meal].id,
+          name: responseData[meal].name,
+          description: responseData[meal].description,
+          price: responseData[meal].price
+        });
+        setIsLoading(false);
+      }
+      setAvailableMeals(loadedMeals);
     };
-    fetchMeals().catch((error) => {
+    fetchMeals().catch(error => {
       setIsLoading(false);
       setHttpError(error.message);
-    }) 
-  },[]);
+    });
+  }, []);
 
-   if(httpError){
+  if (httpError) {
     return (
-      <section className = {classes.mealsLoadingError}>
+      <section className={classes.mealsLoadingError}>
         <p>{httpError}</p>
       </section>
     );
   }
 
-  if(isLoading){
+  if (isLoading) {
     return (
-      <section className = {classes.mealsLoading}>
+      <section className={classes.mealsLoading}>
         <p>Loading...</p>
       </section>
     );
   }
 
-  const mealItem = availableMeals.map((meal) => (
+  const mealItem = availableMeals.map(meal => (
     <MealItem
       key={meal.id}
       id={meal.id}
